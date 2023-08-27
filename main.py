42d90e94c6283334d63ff3a314fc321bb5c3efbb
Zyanya Garcia <zyanyagarcia8@gmail.com>
2023-08-26 19:06:42 -0700

    completed main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    m_stud = int(input('Enter number of male students: '))
+    f_stud = int(input('Enter number of female students: '))
+
+    total = (m_stud + f_stud)
+
+    m_perc = (m_stud / total) * 100
+    f_perc = (f_stud / total) * 100
+
+    print(f"Total Numbers of Students: \t {total}")
+    print(f"Number of Male & Female Students: \t {m_stud} \t {f_stud}")
+    print(f"Percentage of Male & Female Students: \t {m_perc: .2f}% \t {f_perc: .2f}%")
+
+
     return m_perc, f_perc
 
 
